ac2d5aa2aca343bdab0dbe2ea5df3396656003f1
Dandara Aryadne <dandaraaryadne@hotmail.com>
2022-01-23 18:38:10 -0300

    estruturando o exercício

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ let name ="Dandara Aryadne"
 // Crie uma variável que receba um número;
 let number = 98
 // Crie uma variável com o ano atual e subtraia pelo número que você guardou;
-let year = 2022
-let ano = year -=number
+let anoAtual = 2022
+let ano = anoAtual - number
 // Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
 console.log (number+ano)
 //Mostre no console a mensagem "Estou mexendo no console.log";
-let text = "Estou mexendo no console.log"
+console.log = "Estou mexendo no console.log"
 // Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
 let quartaFeira = true
 console.log (quartaFeira)
 // Exiba no console o tipo de dado da váriavel quartaFeira;
-console.log (typeof quartaFeira)
+console.log (quartaFeira)
 
+//Sem os enunciados// 
 
+let name ="Dandara Aryadne"
+let number = 98
+let anoAtual = 2022
+let ano = anoAtual - number
+console.log (number+ano)
+console.log ("Estou mexendo no console.log")
+let quartaFeira = true
+console.log(quartaFeira)
 
 
